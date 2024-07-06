5892bb92201364b51bb6841197a48428f2b52ec9
wanghan8866 <w926425@gmail.com>
2024-07-06 21:06:21 +0100

    :sparkles: add player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    Vector2 rawInput;
+    [SerializeField] float moveSpeed = 10;
+
+    // top left bottom right
+    [SerializeField] float [] paddings = new float [4];
+    Vector2 minBound;
+    Vector2 maxBound;
+    
+    private void Start() {
+        InitBounds();
+    }
     void Update()
     {
+        Move();
+    }
+
+    void InitBounds(){
+        Camera mainCamera = Camera.main;
+        minBound = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBound = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
+    private void Move()
+    {
+        Vector3 delta = rawInput;
+        Vector3 newPosition = transform.position + delta * Time.deltaTime * moveSpeed;
+        newPosition.x = Mathf.Clamp(newPosition.x, minBound.x + paddings[1], maxBound.x - paddings[3]);
+        newPosition.y = Mathf.Clamp(newPosition.y, minBound.y + paddings[2], maxBound.y - paddings[0]);
         
+        transform.position = newPosition;
+    
+    }
+
+    void OnMove(InputValue value){
+        rawInput = value.Get<Vector2>();
+        // Debug.Log(rawInput);
     }
 }
